e95075238c2b3c49038ee4f9fa44dd73cfd2483e
jelyck <jenslycke@gmail.com>
2023-09-26 23:15:53 +0200

    initial springboot application

---
+++ src/main/groovy/io/jelyck/groovywordservice/GroovyWordServiceApplication.groovy
@@ -0,0 +1,13 @@
+package io.jelyck.groovywordservice
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class GroovyWordServiceApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(GroovyWordServiceApplication, args)
+    }
+
+}
---
+++ src/test/groovy/io/jelyck/groovywordservice/GroovyWordServiceApplicationTests.groovy
@@ -0,0 +1,13 @@
+package io.jelyck.groovywordservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GroovyWordServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
